e102aeab2cc460b874315283e094ebaa43d92b23
IDEAMIA-TECH <155779384+IDEAMIA-TECH@users.noreply.github.com>
2025-04-07 17:59:44 -0600

    Update properties.php

--- TEST/modules/admin/properties.php
+++ TEST/modules/admin/properties.php
@@ -754,13 +754,13 @@ $properties = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <div class="modal-dialog modal-xl">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Property Details</h5>
+                    <h5 class="modal-title text-white">Property Details</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
                 </div>
                 <div class="modal-body">
                     <div class="row">
                         <div class="col-md-12">
-                            <h6>Basic Information</h6>
+                            <h6 class="text-white">Property Details</h6>
                             <table class="table table-sm">
                                 <tr>
                                     <th>ID:</th>

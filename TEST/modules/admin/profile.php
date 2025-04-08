4fe980f1c79f86868e976a80d9687c467a7682d8
IDEAMIA-TECH <155779384+IDEAMIA-TECH@users.noreply.github.com>
2025-04-07 23:11:41 -0600

    profile

--- TEST/modules/admin/profile.php
+++ TEST/modules/admin/profile.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../../includes/config.php';
+require_once __DIR__ . '/../../config.php';
 require_once __DIR__ . '/../../includes/auth.php';
 
 // Verificar autenticación
